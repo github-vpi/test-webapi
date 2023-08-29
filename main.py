c7481c9051d8212d92a076038e90a860306cb554
github-vpi <141387254+github-vpi@users.noreply.github.com>
2023-08-29 15:27:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -35,32 +35,32 @@ async def qdrant():
     #return {"message": "Hello World", "Note": "This is a test 1"}
 
 
-@app.get('/qdrantkey')
-async def qdrantkey(qdrant-api-key: str = Query(None, alias="qdrant-api-key")):
+# @app.get('/qdrantkey')
+# async def qdrantkey(qdrant-api-key: str = Query(None, alias="qdrant-api-key")):
 
-    CD_QDRANT_URL = 'https://2aaeafec-b03e-4545-9b12-f8f806ad320a.eu-central-1-0.aws.cloud.qdrant.io:6333'
-    CD_QDRANT_API_KEY = qdrant-api-key
-    # CD_QDRANT_API_KEY = 'QoB7detTXir9bCdMhGNP9tPdWs61VUWChrH9tROY6YXgo4wtD6LNCg'
-    collection_name = 'context'
+#     CD_QDRANT_URL = 'https://2aaeafec-b03e-4545-9b12-f8f806ad320a.eu-central-1-0.aws.cloud.qdrant.io:6333'
+#     CD_QDRANT_API_KEY = qdrant-api-key
+#     # CD_QDRANT_API_KEY = 'QoB7detTXir9bCdMhGNP9tPdWs61VUWChrH9tROY6YXgo4wtD6LNCg'
+#     collection_name = 'context'
     
-    qdrant_client = QdrantClient(url=CD_QDRANT_URL, api_key=CD_QDRANT_API_KEY)
+#     qdrant_client = QdrantClient(url=CD_QDRANT_URL, api_key=CD_QDRANT_API_KEY)
     
-    all_points = qdrant_client.scroll(
-                    collection_name=collection_name, 
-                    limit=1000,
-                    with_payload=True,
-                    with_vectors=False,
-                )
-    full_txt = ''
-    for i in range(len(all_points[0])):
-        full_txt += all_points[0][i].payload['page_content'] + '\n'
+#     all_points = qdrant_client.scroll(
+#                     collection_name=collection_name, 
+#                     limit=1000,
+#                     with_payload=True,
+#                     with_vectors=False,
+#                 )
+#     full_txt = ''
+#     for i in range(len(all_points[0])):
+#         full_txt += all_points[0][i].payload['page_content'] + '\n'
     
-    response = {}
-    response['context'] = full_txt
-    #json_string = json.dumps(response)
-    #print(response)
-    return response
-    #return {"message": "Hello World", "Note": "This is a test"}
+#     response = {}
+#     response['context'] = full_txt
+#     #json_string = json.dumps(response)
+#     #print(response)
+#     return response
+#     #return {"message": "Hello World", "Note": "This is a test"}
 
 @app.get('/test')
 async def test(message: str = Query(None, alias="message")):
